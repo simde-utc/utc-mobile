a04c9a5a3bb9f68988860c99cce3b5672e43fb8e
Alex <abrasseur.pro@gmail.com>
2018-07-14 00:46:01 +0200

    Add Redux

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './src/redux/store';
 import WelcomeScreen from './src/screens/Welcome';
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<WelcomeScreen />
+			<Provider store={ store }>
+				<WelcomeScreen />
+			</Provider>
 		);
 	}
 }
