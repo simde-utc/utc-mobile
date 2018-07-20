0d32ef36b75e19e8849cbc52a1a4dc8355624c73
Alex <abrasseur.pro@gmail.com>
2018-07-21 01:33:33 +0200

    Add HeaderView

--- src/screens/Welcome/SetPreferences.js
+++ src/screens/Welcome/SetPreferences.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { View, Image, Text } from 'react-native';
 import styles from '../../styles'
 
-import BigCheckBox from '../../components/Bigcheckbox';
+// Components
+import BigCheckBox from '../../components/BigCheckBox';
+import HeaderView from '../../components/HeaderView';
 
 export default class SetPreferencesScreen extends React.Component {
 	render() {
+		const BigCheckBoxStyle = [ styles.grayText, {textAlign: 'center'} ]
 		return (
 			<View style={[styles.container, {justifyContent: 'flex-start', alignItems: 'stretch'}, {flex: 1}]}>
-				<View style={[styles.container, styles.lightBlueBg, {flex: 4, justifyContent : 'center', padding: 7}]}>
-					<Text style={[ styles.yellowText , styles.hugeText, {textAlign: 'center', marginTop: 13} ]}>
-						Nous aimerions mieux vous connaître
-					</Text>
-					<Text style={[ styles.whiteText, styles.bigText, { textAlign: 'center' } ]}>
-						Cela nous permettra de paramétrer au mieux l'application selon vos préférences
-					</Text>
-				</View>
+				<HeaderView
+					title="Nous aimerions mieux vous connaître"
+					subtitle="Cela nous permettra de paramétrer au mieux l'application selon vos préférences"
+				/>
 				<View style={[styles.container, styles.whiteBg, {flex: 6, justifyContent: 'center'}]}>
-					<BigCheckBox checked={true} style={[ styles.grayText, {textAlign: 'center'} ]} label={"Afficher la vie associative"} width={250} onChange={(checked) => console.log('I am checked', checked)}/>
-					<BigCheckBox style={[ styles.grayText, {textAlign: 'center'} ]} label={"Afficher les actualités UTC"} width={250}/>
-					<BigCheckBox style={[ styles.grayText, {textAlign: 'center'} ]} label={"Etes-vous cotisant BDE?"} width={250}/>
+					<BigCheckBox checked={true} style={BigCheckBoxStyle} 
+						label={"Afficher la vie associative"} width={250}
+						onChange={(checked) => console.log('I am checked', checked)}/>
+					<BigCheckBox style={BigCheckBoxStyle} 
+						label={"Afficher les actualités UTC"} width={250} />
+					<BigCheckBox style={BigCheckBoxStyle} 
+						label={"Etes-vous cotisant BDE?"} width={250} />
 				</View>
 			</View>
 		);
