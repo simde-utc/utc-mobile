bd21f74500a8bdc288f72283d1bdc387a4382cb7
Alex <abrasseur.pro@gmail.com>
2018-07-21 01:10:36 +0200

    Split WelcomeScreen in Welcome folder

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import store from './src/redux/store';
-import WelcomeScreen from './src/screens/Welcome';
+
+import WelcomeScreen from './src/screens/Welcome/';
 
 export default class App extends React.Component {
 	render() {
---
+++ src/screens/Welcome/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Image, Text, ScrollView, Dimensions } from 'react-native';
+import styles from '../../styles'
+import { colors } from '../../styles/variables';
+
+var Carousel = require('react-native-carousel');
+
+import WelcomeMessageScreen from './WelcomeMessage';
+import SetPreferencesScreen from './SetPreferences';
+
+export default class WelcomeScreen extends React.Component {
+	render() {
+		return (
+			<Carousel 
+				indicatorOffset={0}
+				inactiveIndicatorColor={colors.gray}
+				indicatorColor={colors.yellow}
+				animate={false}
+				indicatorSize={70}
+			>
+				<WelcomeMessageScreen />
+				<SetPreferencesScreen />
+				<View style={styles.container}>
+					<Text>Connexion</Text>
+				</View>
+			</Carousel>
+		);
+
+		/*
+		AUTRE SOLUTION : Avec ScrollView
+		const width = Dimensions.get('window').width
+		const imageLayout = {
+			width: 	width,
+			height: width/2,
+		}
+		return (
+			<ScrollView pagingEnabled directionalLockEnabled horizontal>
+				<Image source={require('../img/logo_utc.png')} style={imageLayout} resizeMode={'center'} />
+				<Image source={require('../img/logo_utc.png')} style={imageLayout} resizeMode={'center'} />
+				<Image source={require('../img/logo_utc.png')} style={imageLayout} resizeMode={'center'} />
+			</ScrollView>			
+		)
+		*/
+	}
+}
