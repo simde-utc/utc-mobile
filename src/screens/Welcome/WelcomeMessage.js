e8ddf433ab4a829bb59f825e7433ced06fb2ebe0
NastuzziSamy <samy@nastuzzi.fr>
2018-07-21 17:32:22 +0200

    Amélioration de l'affichage de bienvenue

--- src/screens/Welcome/WelcomeMessage.js
+++ src/screens/Welcome/WelcomeMessage.js
@@ -6,9 +6,9 @@ export default class WelcomeMessageScreen extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Image source={require('../../img/logo_utc.png')} style={{ width: 200, height: 150 }} resizeMode={'center'} />
-				<Text style={[ styles.yellowText , styles.hugeText, { textAlign: 'center' } ]}>Bienvenue !</Text>
-				<Text style={[ styles.grayText, styles.largeText, { textAlign: 'center' } ]}>Découvrons ensemble l'application</Text>
+				<Image source={require('../../img/logo_utc.png')} style={{ width: 200, height: 150, marginBottom: 50 }} resizeMode={'center'} />
+				<Text style={[ styles.yellowText , styles.hugeText, { textAlign: 'center', marginBottom: 25 } ]}>Bienvenue !</Text>
+				<Text style={[ styles.grayText, styles.largeText, { textAlign: 'center', marginBottom: 75 } ]}>Découvrons ensemble l'application</Text>
 			</View>
 		);
 	}
