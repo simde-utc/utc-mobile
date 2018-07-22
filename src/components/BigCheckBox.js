7e9d2941f85d859a2a8de6fd8e4545899764a040
Alex <abrasseur.pro@gmail.com>
2018-07-22 15:25:27 +0200

    Get CheckBox state from SetPreferences
    Buggy

--- src/components/BigCheckBox.js
+++ src/components/BigCheckBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppRegistry, TouchableHighlight, View, Image, Text} from 'react-native';
+import { TouchableHighlight, View } from 'react-native';
 
 import CheckBox from 'react-native-checkbox-svg';
 import { colors } from '../styles/variables';
@@ -7,32 +7,27 @@ import styles from '../styles'
 
 
 export default class BigCheckBox extends React.Component {
-	constructor() {
-		super();
-		this.state = { _checked: false};
-		this.onPressButton = this.onPressButton.bind(this)
-	}
-
-	onPressButton() {
-		this.setState(
-			previousState => ({ _checked: !previousState._checked }),
-			() => this.props.onChange(this.state._checked)
-		);
-	}
-
 	render() {
 		const viewStyle = {
 			borderColor: colors.gray,
 			borderWidth: 1,
 			borderRadius: 50,
 			padding: 15,
-			width: this.props.width
+			margin: 5,
+			alignItems: 'center',
+			justifyContent: 'center',
+			width: this.props.width || 250
 		}
 		return (
 			<TouchableHighlight onPress={this.onPressButton} underlayColor={"#fff0"}>
 				<View style={viewStyle}>
-					<CheckBox onChange={this.onPressButton} checked={this.props.checked}
-						label={this.props.label} labelStyle={this.props.labelStyle} style={this.props.style}/>
+					<CheckBox
+						checked={ this.props.checked } 
+						onChange={ (checked) => this.props.onChange(checked) }
+						label={ this.props.label }
+						labelBefore={ true }
+						style={ this.props.style }
+					/>
 				</View>
 			</TouchableHighlight>
 		);
