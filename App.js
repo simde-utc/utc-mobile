c40bed01129c1b80beb9c1a7aa97ba438737be0a
NastuzziSamy <samy@nastuzzi.fr>
2018-07-22 18:04:28 +0200

    Mise en place du système de navigation et de routes

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
+import { createStackNavigator } from 'react-navigation';
 
 import WelcomeScreen from './src/screens/Welcome/';
+import ConnectionScreen from './src/screens/Connection/';
+import ConnectedScreen from './src/screens/Connected/';
+import HomeScreen from './src/screens/Home/';
 
+const RootStack = createStackNavigator(
+	{
+		Welcome: WelcomeScreen,
+		Connection: ConnectionScreen,
+		Connected: ConnectedScreen,
+
+		Home: HomeScreen,
+	},
+	{
+		initialRouteName: 'Welcome',
+	}
+);
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Provider store={ store }>
+				<RootStack />
+			</Provider>
+		);
+	}
+}
+/*
 export default class App extends React.Component {
 	render() {
 		return (
@@ -13,3 +40,4 @@ export default class App extends React.Component {
 		);
 	}
 }
+*/
