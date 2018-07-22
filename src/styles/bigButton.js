7457448e08cd4c014cc19fd8eece3e909551beab
NastuzziSamy <samy@nastuzzi.fr>
2018-07-22 16:10:08 +0200

    Ajout et harmonisation des gros bouttons

---
+++ src/styles/bigButton.js
@@ -0,0 +1,11 @@
+import { colors } from './variables';
+
+export const bigButton = {
+	borderColor: colors.gray,
+	borderWidth: 1,
+	borderRadius: 50,
+	padding: 15,
+	margin: 5,
+	justifyContent: 'center',
+	width: 250
+}
