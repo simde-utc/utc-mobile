b4dd739300f4fd40c36cb90c97c9f5f939907df3
Alex <abrasseur.pro@gmail.com>
2018-07-22 15:25:27 +0200

    Get CheckBox state from SetPreferences
    Buggy

--- src/screens/Welcome/SetPreferences.js
+++ src/screens/Welcome/SetPreferences.js
@@ -7,6 +7,33 @@ import BigCheckBox from '../../components/BigCheckBox';
 import HeaderView from '../../components/HeaderView';
 
 export default class SetPreferencesScreen extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			checked: {
+				assoLife: true,
+				utcNews: false,
+				bdeContributor: false,
+			}
+		};
+		this.toggleCheck = this.toggleCheck.bind(this);
+	}
+
+	toggleCheck(key, force = null) {
+		// DEBUG
+		console.log("IIIIIIINNNN")
+		console.log(this.state.checked)
+		return this.setState(prevState => ({
+			...prevState,
+			checked: {
+				...prevState.checked,
+				[key]: force == null ? !prevState.checked[key] : force
+			}
+			// DEBUG
+		}), () => console.log(this.state.checked)
+		);
+	}
+
 	render() {
 		return (
 			<View style={styles.containerStretched}>
@@ -15,13 +42,21 @@ export default class SetPreferencesScreen extends React.Component {
 					subtitle="Cela nous permettra de paramétrer au mieux l'application selon vos préférences"
 				/>
 				<View style={[styles.container, styles.whiteBg, {flex: 7, justifyContent: 'center'}]}>
-					<BigCheckBox checked={true} style={styles.bigCheckBox} labelStyle={styles.h4}
-						label={"Afficher la vie associative"} width={250}
-						onChange={(checked) => console.log('I am checked', checked)}/>
-					<BigCheckBox style={styles.bigCheckBox} labelStyle={styles.h4}
-						label={"Afficher les actualités UTC"} width={250} />
-					<BigCheckBox style={styles.bigCheckBox} labelStyle={styles.h4}
-						label={"Etes-vous cotisant BDE?"} width={250} />
+					<BigCheckBox checked={ this.state.checked.assoLife }
+						labelStyle={styles.h4} width={250}
+						label={"Afficher la vie associative"}
+						onChange={() => this.toggleCheck('assoLife')}
+					/>
+					<BigCheckBox checked={ this.state.checked.utcNews } 
+						labelStyle={styles.h4} width={250}
+						label={"Afficher les actualités UTC"}
+						onChange={() => this.toggleCheck('utcNews')}
+					/>
+					<BigCheckBox checked={ this.state.checked.bdeContributor } 
+						labelStyle={styles.h4} width={250}
+						label={"Etes-vous cotisant BDE?"}
+						onChange={() => this.toggleCheck('bdeContributor')}
+					/>
 				</View>
 			</View>
 		);
