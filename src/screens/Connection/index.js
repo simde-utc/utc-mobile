680e5df605fde96f23feb788cb21b155997571f9
NastuzziSamy <samy@nastuzzi.fr>
2018-07-22 18:54:20 +0200

    Ajout de la page de connexion (sans les requêtes)

--- src/screens/Connection/index.js
+++ src/screens/Connection/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { View, Image, Text, ScrollView, Dimensions } from 'react-native';
+import { View, Image, Text, TextInput } from 'react-native';
 import styles from '../../styles'
-import { colors } from '../../styles/variables';
+
+// Components
+import BigCheckBox from '../../components/BigCheckBox';
+import BigButton from '../../components/BigButton';
+import HeaderView from '../../components/HeaderView';
 
 export default class ConnectionScreen extends React.Component {
 	static navigationOptions = {
@@ -11,9 +15,36 @@ export default class ConnectionScreen extends React.Component {
 		}
 	};
 
+	connect() {
+		return
+	}
+
 	render() {
 		return (
-			<Text> Connexion </Text>
+			<View style={styles.containerStretched}>
+				<HeaderView
+					title="Connectez-vous"
+					subtitle="Il est nécessaire pour le bon fonctionnement de l'application que vous vous connectiez au Portail des Assos. Si vous ne vous y êtes jamais connecté, veuillez vous connecter en tant que CAS"
+				/>
+				<View style={[styles.container, styles.whiteBg, {flex: 7, justifyContent: 'center', marginTop: 50, marginBottom: 75 }]}>
+					<TextInput style={ styles.bigButton }
+						placeHolder="Email / Login CAS"
+						keyboardType="email-address"
+						autoCapitalize="none"
+						autoCorrect={ false }
+					/>
+					<TextInput style={ styles.bigButton }
+						placeHolder="Mot de passe"
+						autoCapitalize="none"
+						autoCorrect={ false }
+						secureTextEntry={ true }
+					/>
+					<BigButton label={ "Se connecter" }
+						style={{ marginTop: 25}}
+						onPress={() => this.connect()}
+					/>
+				</View>
+			</View>
 		);
 	}
 }
