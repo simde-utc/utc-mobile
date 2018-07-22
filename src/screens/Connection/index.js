187285c51e29e871158c26238a4f680042f5fb24
NastuzziSamy <samy@nastuzzi.fr>
2018-07-23 00:18:26 +0200

    Mise en place de la connexion et des champs

--- src/screens/Connection/index.js
+++ src/screens/Connection/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { View, Image, Text, TextInput } from 'react-native';
+import React from 'react'
+import { View, Image, Text, TextInput } from 'react-native'
+import Button from 'react-native-button'
 import styles from '../../styles'
 
 // Components
-import BigCheckBox from '../../components/BigCheckBox';
-import BigButton from '../../components/BigButton';
-import HeaderView from '../../components/HeaderView';
+import BigCheckBox from '../../components/BigCheckBox'
+import BigButton from '../../components/BigButton'
+import HeaderView from '../../components/HeaderView'
+
+// API
+import PortailApi from '../../services/Portail'
 
 export default class ConnectionScreen extends React.Component {
 	static navigationOptions = {
@@ -15,11 +19,20 @@ export default class ConnectionScreen extends React.Component {
 		}
 	};
 
-	connect() {
-		return
+	constructor (props) {
+		super(props)
+
+		this.state = {
+			emailOrLogin: '',
+			password: ''
+		}
 	}
 
-	render() {
+	connect () {
+		PortailApi.login(this.state.emailOrLogin, this.state.password)
+	}
+
+	render () {
 		return (
 			<View style={styles.containerStretched}>
 				<HeaderView
@@ -29,20 +42,29 @@ export default class ConnectionScreen extends React.Component {
 				<View style={[styles.container, styles.whiteBg, {flex: 7, justifyContent: 'center', marginTop: 50, marginBottom: 75 }]}>
 					<TextInput style={ styles.bigButton }
 						placeHolder="Email / Login CAS"
+						value={ this.state.emailOrLogin }
+						onChangeText={(text) => this.setState(() => { return { emailOrLogin: text } })}
 						keyboardType="email-address"
 						autoCapitalize="none"
 						autoCorrect={ false }
 					/>
 					<TextInput style={ styles.bigButton }
 						placeHolder="Mot de passe"
+						value={ this.state.password }
+						onChangeText={(text) => this.setState(() => { return { password: text } })}
 						autoCapitalize="none"
 						autoCorrect={ false }
 						secureTextEntry={ true }
 					/>
 					<BigButton label={ "Se connecter" }
-						style={{ marginTop: 25}}
+						style={{ marginTop: 25, marginBottom: 15 }}
 						onPress={() => this.connect()}
 					/>
+					<Button style={ styles.lightBlueText }
+						onPress={ (checked) => this.props.navigation.navigate('Connected') }
+					>
+						Je ne souhaite pas me connecter
+					</Button>
 				</View>
 			</View>
 		);
