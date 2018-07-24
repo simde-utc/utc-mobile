dbcf117dcadf3cdc6bb93013dba7e6f2a21e3702
NastuzziSamy <samy@nastuzzi.fr>
2018-07-24 17:26:18 +0200

    Ajout d'une coniguration de base pour l'affichage

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import { View, Image, Text, ScrollView, Dimensions } from 'react-native';
 
-import BlockManager from '../../components/Block'
+import BlockHandler from '../../components/Block'
 
 import styles from '../../styles'
 import { colors } from '../../styles/variables';
+
+const config = [
+    {
+        children: (
+            <Text style={ styles.bg.yellow }>Lorem Ipsum !!</Text>
+        ),
+        extend: true,
+        style: styles.bg.lightGray
+    },
+    {
+        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+        style: styles.bg.lightGray
+    },
+    [
+        {
+            children: (
+                <Text>0</Text>
+            ),
+            style: styles.bg.lightGray
+        },
+		{
+            children: (
+                <Text>1</Text>
+            ),
+            style: styles.bg.lightGray
+        },
+        {
+            children: (
+                <Text>2</Text>
+            ),
+			extend: true,
+            style: styles.bg.lightGray
+        },
+    ],
+	{
+		image: require('../../img/logo_utc.png'),
+		style: styles.bg.lightGray
+	},
+]
 
 export default class HomeScreen extends React.Component {
 	static navigationOptions = {
@@ -13,8 +52,9 @@ export default class HomeScreen extends React.Component {
 
 	render () {
 		return (
-			<BlockManager
-				style={ styles.bg.yellow }
+			<BlockHandler
+				style={{ backgroundColor: '#0f0' }}
+				blocks={ config }
 			/>
 		);
 	}
