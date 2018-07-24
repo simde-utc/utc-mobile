305383f4106c469d830d9fbaf9a58844d7dba4f6
NastuzziSamy <samy@nastuzzi.fr>
2018-07-24 21:47:47 +0200

    Ajout d'un exemple d'action lors d'un click sur un block

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,51 +6,53 @@ import BlockHandler from '../../components/Block'
 import styles from '../../styles'
 import { colors } from '../../styles/variables';
 
-const config = [
-    {
-        children: (
-            <Text style={ styles.bg.yellow }>Lorem Ipsum !!</Text>
-        ),
-        extend: true,
-        style: styles.bg.lightGray,
-		onPress: () => {
-			console.log('Lorem Ipsum')
-		}
-    },
-    {
-        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-        style: styles.bg.lightGray
-    },
-    [
-        {
-            text: 'SiMDE',
-            style: styles.bg.lightGray
-        },
-		{
-            text: 'MapUTC',
-            style: styles.bg.lightGray
-        },
-        {
-            children: (
-				<Text style={[ styles.text.h2, styles.text.center ]}>12 €</Text>
-			),
-			image: require('../../img/payutc.png'),
-			extend: true,
-            style: styles.bg.lightGray
-        },
-    ],
-	{
-		image: require('../../img/logo_utc.png'),
-		style: styles.bg.lightGray
-	},
-]
-
 export default class HomeScreen extends React.Component {
 	static navigationOptions = {
 		title: 'Accueil',
 	};
 
 	render () {
+		const navigation = this.props.navigation
+
+		const config = [
+		    {
+		        children: (
+		            <Text style={ styles.bg.yellow }>Je veux voir mon intro !!</Text>
+		        ),
+		        extend: true,
+		        style: styles.bg.lightGray,
+				onPress: function () {
+					navigation.navigate('Welcome')
+				}
+		    },
+		    {
+		        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+		        style: styles.bg.lightGray
+		    },
+		    [
+		        {
+		            text: 'SiMDE',
+		            style: styles.bg.lightGray
+		        },
+				{
+		            text: 'MapUTC',
+		            style: styles.bg.lightGray
+		        },
+		        {
+		            children: (
+						<Text style={[ styles.text.h2, styles.text.center ]}>12 €</Text>
+					),
+					image: require('../../img/payutc.png'),
+					extend: true,
+		            style: styles.bg.lightGray
+		        },
+		    ],
+			{
+				image: require('../../img/logo_utc.png'),
+				style: styles.bg.lightGray
+			},
+		]
+
 		return (
 			<BlockHandler
 				style={{ backgroundColor: '#0f0' }}
