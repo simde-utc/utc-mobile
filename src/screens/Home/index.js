f07e54222af4e386ceeec55ff039dfa50ad6005a
NastuzziSamy <samy@nastuzzi.fr>
2018-07-25 21:35:42 +0200

    Début de mise en place des blocs dans le menu d'accueil

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, Image, Text, ScrollView, Dimensions } from 'react-native';
+
 import styles from '../../styles'
 import { colors } from '../../styles/variables';
+
+import { BlockGrid } from '../../components/Block'
 
 export default class HomeScreen extends React.Component {
 	static navigationOptions = {
@@ -10,7 +13,12 @@ export default class HomeScreen extends React.Component {
 
 	render() {
 		return (
-			<Text> Home </Text>
+			<ScrollView style={{ width: '100%', height: '100%', backgroundColor: '#F00' }}>
+				<BlockGrid style={[ styles.container.default, { backgroundColor: '#0F0' } ]}
+					data={[ 1, 2, 3 ]}
+				>
+				</BlockGrid>
+			</ScrollView>
 		);
 	}
 }
--- src/screens/MainLayout.js
+++ src/screens/MainLayout.js
@@ -3,10 +3,9 @@ import { createBottomTabNavigator  } from 'react-navigation';
 import { View, Text } from 'react-native';
 import styles from '../styles/'
 
-// import HomeScreen from './Home/';
+import HomeScreen from './Home';
 
 const show = (text) => <View style={ styles.container.center }><Text style={ styles.text.h0}>{ text }</Text></View>
-const HomeScreen = () => show('Home')
 const EventsScreen = () => show('Events')
 const AgendaScreen = () => show('Agenda')
 const NotificationsScreen = () => show('Notifications')
