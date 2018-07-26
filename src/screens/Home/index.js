b98210443a230f2d3b6b9dc8e89ef3a94239378d
NastuzziSamy <samy@nastuzzi.fr>
2018-07-27 01:37:14 +0200

    Ajout de la gestion d'ajout de blocs

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -11,51 +11,73 @@ export default class HomeScreen extends React.Component {
 		title: 'Accueil',
 	};
 
+	constructor (props) {
+		super(props)
+
+		this.state = {
+			config: [
+			    {
+			        children: (
+			            <Text>Je veux voir mon intro !!</Text>
+			        ),
+			        extend: true,
+					onPress: function () {
+						navigation.navigate('Welcome')
+					}
+			    },
+			    [
+					{
+					},
+					{
+						text: "Samy",
+					}
+				],
+			    [
+			        {
+			            text: 'SiMDE',
+			        },
+					{
+			            text: 'MapUTC',
+			        },
+			        {
+			            children: (
+							<Text style={[ styles.text.h2, styles.text.center ]}>12 €</Text>
+						),
+						image: require('../../img/payutc.png'),
+						extend: true,
+			        },
+			    ],
+				{
+					image: require('../../img/logo_utc.png'),
+				},
+			]
+		}
+	}
+
+	addNewBlock (index) {
+		const toAdd = {
+			text: 'Je suis nouveau !'
+		}
+
+		this.setState((prevState) => {
+			if (Array.isArray(prevState.config[index[0]]))
+				prevState.config[index[0]][index[1]] = toAdd
+			else
+				prevState.config[index[0]] = toAdd
+
+			return prevState
+		})
+	}
+
 	render () {
 		const navigation = this.props.navigation
 
-		const config = [
-		    {
-		        children: (
-		            <Text>Je veux voir mon intro !!</Text>
-		        ),
-		        extend: true,
-				onPress: function () {
-					navigation.navigate('Welcome')
-				}
-		    },
-		    [
-				{
-				},
-				{
-					text: "Samy je te jure <3",
-				}
-			],
-		    [
-		        {
-		            text: 'SiMDE',
-		        },
-				{
-		            text: 'MapUTC',
-		        },
-		        {
-		            children: (
-						<Text style={[ styles.text.h2, styles.text.center ]}>12 €</Text>
-					),
-					image: require('../../img/payutc.png'),
-					extend: true,
-		        },
-		    ],
-			{
-				image: require('../../img/logo_utc.png'),
-			},
-		]
-
 		return (
 			<BlockHandler
-				blocks={ config }
+				blocks={ this.state.config }
 				editMode={ true }
 				deleteMode={ true }
+				onPressNewBlock={ this.addNewBlock.bind(this) }
 			/>
 		);
 	}
