754402a70da9045ca4637f000f560391e3cd787c
Alex <abrasseur.pro@gmail.com>
2018-07-26 21:29:25 +0200

    Refactor AppLoader

--- src/screens/AppLoader.js
+++ src/screens/AppLoader.js
@@ -2,22 +2,13 @@ import React from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import styles from '../styles/';
 import { colors } from '../styles/variables';
+import PortailApi from '../services/Portail';
 
 export default class AppLoaderScreen extends React.Component {
-	componentDidMount() {
-		// Fetch tokens...
-		let data = {
-			isConnected: true
-		}
-
-		// DEBUG
-		setTimeout(this.onAppLoaded.bind(this, data), 500);
-	}
-
-	onAppLoaded(data) {
-		this.props.navigation.navigate(
-			data.isConnected ? 'Main' : 'Welcome'
-		);
+	componentWillMount() {
+		this.bootstrap()
+			.then(this.appLoaded.bind(this))
+			.catch(error => this.handleError.bind(this, error))
 	}
 
 	render() {
@@ -27,5 +18,38 @@ export default class AppLoaderScreen extends React.Component {
 				<Text>Chargement de l'application</Text>
 			</View>
 		);
+	}
+
+
+	// ========== App Bootstrapping Methods ==========
+
+	// Load async data and store it in the App store
+	bootstrap = async () => {
+		// Fetch tokens from SecureStore...
+		console.log("=== App Bootstrapping")
+		this.setState(prevState => ({
+			...prevState,
+			isConnected: PortailApi.isConnected()
+		}));
+
+		// Download fonts, images etc...
+
+		// Must return a Promise
+		return new Promise(resolve => setTimeout(() => {
+			console.log("=== App Bootstrapped")
+			resolve()
+		}, 500))
+	}
+
+
+	appLoaded = () => {
+		console.log("=== App Loaded")
+		this.props.navigation.navigate(
+			this.state.isConnected ? 'Main' : 'Welcome'
+		);
+	}
+
+	handleError(error) {
+		console.warn(error)
 	}
 }
