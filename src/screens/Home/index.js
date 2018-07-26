76d1a048224118836d7d2f82bcd80eb077c1bf4b
Alex <abrasseur.pro@gmail.com>
2018-07-27 00:14:21 +0200

    Fix Storage, can set all and get objects and arrays

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,34 +6,39 @@ import Storage from '../../services/Storage';
 
 const key = "test storage"
 
+
 export default class HomeScreen extends React.Component {
 
-	componentWillMount() {
-		this.getData()
-	}
-
-	getData = async () => {
+	getter = async () => {
+		console.log("Getter started")
 		Storage.getItem(key)
 			.then(data => console.log("Got ", data))
 			.catch(err => console.log(err))
 	}
 
-	setData = async (i) => {
+	setter = async (i) => {
+		console.log("Setter "+i+" started")
 		let data;
 		switch (i) {
 			case 1:
-				data = "abc"
+				data = "abc";
+				break;
 			case 2:
-				data = ["azd", 4, { a: 4 }]
+				data = ["azd", 4, 4.5];
+				break;
 			case 3:
-				data = {a: 1, b: 3}
+				data = {a: 1, b: 3};
+				break;
 			case 4:
-				data = null
+				data = null;
+				break;
 			case 5:
-				data = undefined
+				data = undefined;
+				break;
 		}
+		console.log("Data to set :", data)
 
-		Storage.setData(key, data)
+		Storage.setItem(key, data)
 			.then(() => console.log("Data "+i+" set"))
 			.catch(err => console.warn("Error setting data "+i+" : "+err))
 	}
@@ -42,12 +47,12 @@ export default class HomeScreen extends React.Component {
 		return (
 			<View style={ styles.container.center }>
 				<Text> Home </Text>
-				<Button onPress={ this.getData } title="Get item and log" />
-				<Button onPress={() => this.setData(1) } title="Set text" />
-				<Button onPress={() => this.setData(2) } title="Set array" />
-				<Button onPress={() => this.setData(3) } title="Set object" />
-				<Button onPress={() => this.setData(4) } title="Set null" />
-				<Button onPress={() => this.setData(5) } title="Set undefined" />
+				<Button onPress={ this.getter } title="Get item and log" />
+				<Button onPress={() => this.setter(1) } title="Set text" />
+				<Button onPress={() => this.setter(2) } title="Set array" />
+				<Button onPress={() => this.setter(3) } title="Set object" />
+				<Button onPress={() => this.setter(4) } title="Set null" />
+				<Button onPress={() => this.setter(5) } title="Set undefined" />
 			</View>
 		);
 	}
