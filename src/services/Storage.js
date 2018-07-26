7dad6869aa572e1c936f8fbd74f67a5dd4b1ee6c
Alex <abrasseur.pro@gmail.com>
2018-07-27 00:14:21 +0200

    Fix Storage, can set all and get objects and arrays

--- src/services/Storage.js
+++ src/services/Storage.js
@@ -1,66 +1,77 @@
 import { AsyncStorage } from "react-native";
 import { SecureStore } from "expo";
 
-export default class Storage {
+class Storage {
 
 	// ========== Normal Storage ==========
-
-	setItem = async (key, value) => {
-		if (!key)	throw "Clef non définie!";
-		if (!value)	throw "Valeur non définie!";
-
-		const data = await stringifyData(value)
-		return AsyncStorage.setItem(key, data);
-	}
 
 	getItem = async (key) => {
 		if (!key)	throw "Clef non définie!";
 
 		try {
 			const data = await AsyncStorage.getItem(key);
+			return this.parseData(data);
 		} catch (err) {
 			throw "Impossible de récupérer les données"
 		}
-		return parseData(data);
 	}
 
+	setItem = async (key, value) => {
+		if (!key)	throw "Clef non définie!";
+		if (!value)	throw "Valeur non définie!";
+
+		const data = await this.stringifyData(value)
+		return AsyncStorage.setItem(key, data);
+	}
+
+
 	// ========== Encrypted Storage ==========
+
+	getSensitiveData = async (key) => {
+		if (!key)	throw "Clef non définie!";
+
+		try {
+			const data = await SecureStore.getItemAsync(key);
+			return this.parseData(data);
+		} catch (err) {
+			throw "Impossible de récupérer les données"
+		}
+	}
 
 	setSensitiveData = async (key, value) => {
 		if (!key)	throw "Clef non définie!";
 		if (!value)	throw "Valeur non définie!";
 
-		try {
-			const data = await stringifyData(value)
-		} catch (err) {
-			throw "Impossible de récupérer les données"
-		}
+		const data = await this.stringifyData(value)
 		return SecureStore.setItemAsync(key, data);
 	}
-
-	getSensitiveData = async (key) => {
-		if (!key)	throw "Clef non définie!";
-
-		const data = await SecureStore.getItemAsync(key);
-		return parseData(data);
-	}
 	
+
 	// ========== JSON Helpers ==========
 
 	stringifyData = async (data) => {
+		// TODO : set entier
+		console.log(typeof data)
 		try {
-			return JSON.stringify(value);
+			if (typeof data === 'string')
+				return data		
+			return JSON.stringify(data);
 		} catch (error) {
+			console.log("error : ", error)
 			throw "Impossible de convertir les données en string."
 		}
 	}
 
 	parseData = async (data) => {
+		// TODO : récupération text simple et entier
 		try {
 			return JSON.parse(data)
 		} catch (error) {
+			console.log("error : ", error)
 			throw  "Impossible de parser les données récupérées."
 		}
 	}
 
 }
+
+export default new Storage();
