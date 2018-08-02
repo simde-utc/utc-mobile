6601bf99c9f09da4240d826161afbe68ff75b007
CdRom1 <r.maliach@live.fr>
2018-08-02 12:16:59 +0200

    fix bugs

--- src/services/ActualitesUTC.js
+++ src/services/ActualitesUTC.js
@@ -1,11 +1,12 @@
 import Api from './Api'
 
-export class ActualitesUTC extends Api {
+export default class ActualitesUTC extends Api {
 
 	static ACTUS_FEED = 'https://actualites.utc.fr/feed/'
 
 	constructor(st) {
 		super(ActualitesUTC.ACTUS_FEED);
+		if(!st) {throw "Pas de service ticket!";}
 		this._st = st;
 		this._loaded = false;
 		this._upToDate = false;
@@ -17,15 +18,14 @@ export class ActualitesUTC extends Api {
 			Api.GET,
 			{},
 			{
-				ticket: this._st;
+				ticket: this._st
 			}
 		).then(([response, status]) => { // Si on a une 20x
 			console.log(response + " --- "+ status);
-		}).catch([response, status]) => {
+		}).catch( ([response, status]) => {
 			console.log(response + " --- "+ status);
 		});
 	}
 
 }
 
-export default new ActualitesUTC();
