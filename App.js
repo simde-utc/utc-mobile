dbf39d19c04a9b4381f3d1a2427791039a4eb7cd
CdRom1 <r.maliach@live.fr>
2018-08-04 16:21:30 +0200

    disable debugging screen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 
-/**
+
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import AppLoader from './src/screens/AppLoader';
 import AppLayout from './src/screens/AppLayout';
@@ -24,19 +24,17 @@ const AppSwitch = createSwitchNavigator({
 }, {
 	initialRouteName: 'Loading'
 })
-**/
 
-import ActualitesMDETestScreen from './src/screens/ActualitesMDETest.js';
 
 
 
 export default class App extends React.Component {
-	render() {/**
+	render() {
 		return (
 			<Provider store={ store }>
 				<AppSwitch />
 			</Provider>
-		);**/
-		return(<ActualitesMDETestScreen />);
+		);
+		
 	}
 }
