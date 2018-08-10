fcb184760a5e5333549e8a027b098651507ab1db
CdRom1 <r.maliach@live.fr>
2018-08-10 14:36:49 +0200

    improve List, Icon

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,21 @@
+/**
+ * Affiche une liste
+ * @author Alexandre Brasseur <alexandre.brasseur@etu.utc.fr>, Romain Maliach-Auguste <r.maliach@live.fr>
+ *
+ * @copyright Copyright (c) 2017, SiMDE-UTC
+ * @license AGPL-3.0
+**/
+
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import Icon from './Icon';
+import News from '../img/icons/news.svg'
+import Map from '../img/icons/map.svg'
+import Bell from '../img/icons/bell.svg'
+import Calendar from '../img/icons/calendar.svg'
+
+const nativeIcons = ["news", "map", "bell", "calendar"]
 
 const listStyle = StyleSheet.create({
 	container: {
@@ -15,7 +31,6 @@ const listStyle = StyleSheet.create({
 		marginHorizontal: 20,
 	},
 	icon: {
-		backgroundColor: '#444',
 		marginRight: 15
 	},
 	text: {
@@ -25,15 +40,42 @@ const listStyle = StyleSheet.create({
 
 export default class List extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this._renderItem = this._renderItem.bind(this);
+		this._isNativeIcon = this._isNativeIcon.bind(this);
+		this._iconKeyToSvg = this._iconKeyToSvg.bind(this);
+	}
+	
 	_keyExtractor(item, index) {
 		return String(index);
 	}
 
+	_iconKeyToSvg(key) {
+		switch (key) {
+			case "news":
+				return News;
+				break;
+			case "map":
+				return Map;
+				break;
+			case "bell":
+				return Bell;
+				break;
+			case "calendar":
+				return Calendar;
+				break;
+		}
+	}
+
+	_isNativeIcon(icon) {
+		return nativeIcons.includes(icon);
+	}
+
 	_renderItem({ item }) {
-		// TODO : icon
 		return (
 			<View style={ listStyle.view }>
-				{ item.icon && <View height={30} width={30} style={ listStyle.icon} /> }
+				<View style={listStyle.icon}>{item.icon && (this._isNativeIcon(item.icon) ? <Icon height={25} width={25} image={this._iconKeyToSvg(item.icon)} /> : <Image source={item.icon} style={{height : "25", width : "25"}}/> )}</View>
 				<Text style={ listStyle.text }>{ item.text }</Text>
 			</View>
 		);
