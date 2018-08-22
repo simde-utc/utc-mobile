46961fed3370ef4fc00d8567f471c72ebf6241ac
CdRom1 <r.maliach@live.fr>
2018-08-22 23:17:30 +0200

    quelques modifications sur le comportement de Hr et préparation de la structure d'Article

--- src/styles/Article.js
+++ src/styles/Article.js
@@ -2,13 +2,14 @@ import { colors } from './variables';
 
 export const article = {
 	container: {
-		height: 100,
 		width: '100%',
 		borderWidth: 1,
+		borderColor: colors.lightGray,
 		marginBottom:10,
-		backgroundColor: 'lightblue',
+		backgroundColor: colors.white,
 		justifyContent: 'flex-start',
 		alignItems: 'center',
+		padding: 5,
 	},
 
 	loadingIndicatorContainer: {
@@ -20,9 +21,13 @@ export const article = {
 		color: colors.lightGray,
 	},
 	articlesFeedContainer: {
-	flex:1,
-	alignItems: 'center',
-	justifyContent: 'center',
-	paddingHorizontal: 10,
+		flex:1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 10,
+		backgroundColor: colors.veryLightGray,
+	},
+	contentContainer: {
+		backgroundColor: 'lightblue',
 	}
 };
