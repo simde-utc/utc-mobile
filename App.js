874a2fdf23d4a131c0d22ef9490dc9799a7d5bdb
NastuzziSamy <samy@nastuzzi.fr>
2018-08-22 23:36:25 +0200

    Amélioration de l'affichage général avec prise en compte de la statusbar

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
+/**
+ * Fichier chargé pour lancer l'application. Définie toutes les routes qui suivent
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ * @author Alexabdre Brasseur <alexandre.brasseur@etu.utc.fr>
+ *
+ * @copyright Copyright (c) 2018, SiMDE-UTC
+ * @license AGPL-3.0
+**/
+
 import React from 'react';
+import { View, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 
-import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator, SafeAreaView } from 'react-navigation';
 import AppLoader from './src/screens/AppLoader';
 import AppLayout from './src/screens/AppLayout';
 import ConnectionScreen from './src/screens/Connection/';
@@ -28,7 +38,12 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<Provider store={ store }>
-				<AppSwitch />
+				<SafeAreaView style={{ flex: 1 }}>
+					<StatusBar
+						translucent={ true }
+					/>
+					<AppSwitch />
+				</SafeAreaView>
 			</Provider>
 		);
 	}
