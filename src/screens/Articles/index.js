80768997c5601c2c5c8d1fd05f71580f9d97df55
NastuzziSamy <samy@nastuzzi.fr>
2018-08-22 20:23:49 +0200

    Mise en place et lien les articles UTC/Portail + affichage chargement

--- src/screens/Articles/index.js
+++ src/screens/Articles/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import { FlatList, Text, View, ActivityIndicator } from 'react-native';
 import styles from '../../styles'
 
+import CASAuth from '../../services/CASAuth';
 import Portail from '../../services/Portail';
+
 import ArticleComponent from '../../components/Articles/Article';
 
 const DEFAULT_ARTICLES_PAGINATION = 25;
@@ -23,9 +25,10 @@ export default class ArticlesScreen extends React.Component {
 		this.state = {
 			page: 0,
 			pagination: DEFAULT_ARTICLES_PAGINATION,
-			canLoadMoreContent: true,
+			canLoadMoreUTCArticles: true,
+			canLoadMorePortailArticles: true,
 			articles: [],
-			isLoading: false,
+			loading: false,
 		};
 
 		this.props.articleHeight = 100;
@@ -36,34 +39,77 @@ export default class ArticlesScreen extends React.Component {
 	}
 
 	_loadMoreContentAsync() {
-		if (!this.state.canLoadMoreContent) return
+		if (!this.state.canLoadMorePortailArticles) return
 
-		this.setState(prevState => ({ ...prevState, isLoading: true }))
+		var promises = []
 
-		Portail.getArticles(this.state.pagination, this.state.page + 1, 'latest').then(([response, status]) => {
-			this.setState(prevState => {
-				prevState.page++
-				prevState.articles = prevState.articles.concat(response)
+		if (CASAuth.isConnected() && this.state.canLoadMoreUTCArticles) {
+			var UTCArticles = this._loadUTCArticles()
+			promises.push(UTCArticles)
+		}
 
-				return prevState
+		if (Portail.isConnected() && this.state.canLoadMorePortailArticles) {
+			var PortailArticles = this._loadPortailArticles()
+			promises.push(PortailArticles)
+		}
+
+		if (promises) {
+			this.setState(prevState => ({ ...prevState, loading: true }))
+			this.flatList.scrollToEnd()
+
+			return new Promise.all(promises).then(([articles, articles2]) => {
+				this.setState(prevState => ({ ...prevState, loading: false }))
+
+				if (!articles)
+					return
+
+				if (articles2)
+					articles.concat(articles2)
+
+				console.log(articles)
+
+				// Il faut trier
+				this.setState(prevState => {
+					prevState.page++
+					prevState.articles = prevState.articles.concat(articles)
+
+					return prevState
+				})
+			}).catch(() => { this.setState(prevState => ({ ...prevState, loading: false })) })
+		}
+	}
+
+	_loadUTCArticles() {
+		return CASAuth.getService(process.env.ACTUS_UTC_FEED_LOGIN).then(([serviceTicket]) => {
+			var actus = new ActualitesUTC(serviceTicket)
+
+			return actus.loadArticles().then(() => {
+				resolve([actus.getArticles(paginate, page, order, week), 200])
+			}).catch(([response, status]) => {
+				this.setState(prevState => ({ ...prevState, canLoadMoreUTCArticles: false }))
 			})
-		}).catch(([response, status]) => {
-			if (status === 416)
-				this.setState(prevState => ({ ...prevState, canLoadMoreContent: false }))
 		})
 	}
 
-
+	_loadPortailArticles() {
+		return Portail.getArticles(this.state.pagination, this.state.page + 1, 'latest').then(([response, status]) => {
+			return response
+		}).catch(([response, status]) => {
+			if (status === 416)
+				this.setState(prevState => ({ ...prevState, canLoadMorePortailArticles: false }))
+		})
+	}
 
 	render() {
 		return (<View style={styles.article.articlesFeedContainer}>
 				<FlatList
+					ref={(component) => ( this.flatList = component )}
 					data={this.state.articles}
 					renderItem={({item}) => <ArticleComponent data={item} />}
 					onEndReached={ this._loadMoreContentAsync.bind(this) }
 					onEndReachedThreshold = {THRESHOLD}
 					keyExtractor={ (item) => item["id"].toString()}
-					ListFooterComponent = {<View style={styles.article.loadingIndicatorContainer}><Text style={styles.article.loadingIndicatorText}>{!this.state.canLoadMoreContent && 'Y\'en a plus!'}</Text></View>}
+					ListFooterComponent = {<View style={ styles.article.loadingIndicatorContainer }>{ this.state.loading && <ActivityIndicator size="small" color="#0000ff" /> }</View>}
 				/>
 			</View>
 		);
