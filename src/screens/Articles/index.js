dd31dab6e052e7d107e639da6500dbd97202c8a8
NastuzziSamy <samy@nastuzzi.fr>
2018-08-23 01:41:27 +0200

    Amélioration du traitement des articles

--- src/screens/Articles/index.js
+++ src/screens/Articles/index.js
@@ -7,6 +7,7 @@ import Portail from '../../services/Portail';
 
 import ActualitesUTC from '../../services/ActualitesUTC';
 
+import Filter from '../../components/Filter';
 import ArticleComponent from '../../components/Articles/Article';
 
 const DEFAULT_ARTICLES_PAGINATION = 6; //debug pour bien vérifier le chargement en plusieurs fois
@@ -47,99 +48,96 @@ export default class ArticlesScreen extends React.Component {
 
 		if (CASAuth.isConnected() && this.state.canLoadMoreUTCArticles) {
 			var UTCArticles = this._loadUTCArticles()
+
 			promises.push(UTCArticles)
 		}
 
 		if (Portail.isConnected() && this.state.canLoadMorePortailArticles) {
 			var PortailArticles = this._loadPortailArticles()
+
 			promises.push(PortailArticles)
 		}
-		
+
 
 		if (promises) {
 			this.setState(prevState => ({ ...prevState, loading: true }))
 
-
-
 			return new Promise.all(promises).then(([articles, articles2]) => {
 				this.setState(prevState => ({ ...prevState, loading: false }))
 
-				if (!(articles || articles2)) {return;}
-				if (articles && articles2) {
-					articles2.forEach(this._normalizePortailArticle);
-					articles = articles.concat(articles2);
-				}					
-				else {
-					articles = articles || articles2;}
+				if (!(articles || articles2)) return
 
-				articles.sort(this._compArtDate);
-				articles.reverse(); //parce qu'on veut les plus récents en haut
+				if (articles && articles2)
+					articles = articles.concat(articles2)
+				else
+					articles = articles || articles2
+
+				articles.sort((article1, article2) => {
+					return new Date(article1.created_at || article1.date_gmt) > new Date(article2.created_at || article2.date_gmt) ? -1 : 1
+				})
 
 				this.setState(prevState => {
 					prevState.page++
 					prevState.articles = prevState.articles.concat(articles)
+
 					return prevState
 				})
-			}).catch((e) => {console.warn(e); this.setState(prevState => ({ ...prevState, loading: false })) })
+			}).catch((e) => {
+				console.warn(e)
+
+				this.setState(prevState => ({ ...prevState, loading: false }))
+			})
 		}
 	}
 
 	_loadUTCArticles() {
+		return CASAuth.getService(process.env.ACTUS_UTC_FEED_LOGIN).then(([serviceTicket]) => {
+			var actus = new ActualitesUTC(serviceTicket)
 
-		return new Promise((resolve, reject) => {
-			CASAuth.getService(process.env.ACTUS_UTC_FEED_LOGIN).then(([serviceTicket]) => {
-				var actus = new ActualitesUTC(serviceTicket)
+			return actus.loadArticles().then(() => {
+				return actus.getArticles(this.state.pagination, this.state.page + 1, 'latest')
+			}).catch(([response, status]) => {
+				switch(status) {
+					case 416:
+						this.setState(prevState => ({ ...prevState, canLoadMoreUTCArticles: false }));
+						break
+					case 523:
+					default:
+						//TODO: afficher réseau ou inconnue
+						console.warn([response, status])
+						this.setState(prevState => ({ ...prevState, canLoadMoreUTCArticles: false }));
+						break
+				}
 
-				return actus.loadArticles().then(() => {
-					resolve(actus.getArticles(this.state.pagination, this.state.page + 1, 'latest'))
-				}).catch(([response, status]) => {
-					switch(status) {
-						case 416:
-							this.setState(prevState => ({ ...prevState, canLoadMoreUTCArticles: false }));
-							break;
-						case 523:
-						default:
-							//TODO: afficher réseau ou inconnue
-							console.warn([response, status]);
-							this.setState(prevState => ({ ...prevState, canLoadMoreUTCArticles: false }));
-							break;
-					}
-							
-				})
-			});
-		});
-
+				return []
+			})
+		}).catch(() => {
+			return []
+		})
 	}
 
 	_loadPortailArticles() {
 		return Portail.getArticles(this.state.pagination, this.state.page + 1, 'latest').then(([response, status]) => {
-			return response
+			return response.map((article) => {
+				article["created_at"] = article["created_at"].replace(' ', 'T')
+
+				return article
+			})
 		}).catch(([response, status]) => {
 			if (status === 416)
 				this.setState(prevState => ({ ...prevState, canLoadMorePortailArticles: false }))
 
 			return []
 		})
-	}
 
-
-	//pure helpers
-
-	_normalizePortailArticle(article) {
-		article["created_at"] = article["created_at"].replace(' ', 'T');
-	}
-
-	_compArtDate(a,b) {
-	var dateA = new Date(a.created_at || a.date_gmt); var dateB = new Date(b.created_at || b.date_gmt);
-	  if (dateA < dateB)
-	    return -1;
-	  if (dateA > dateB)
-	    return 1;
-	  return 0;
 	}
 
 	render() {
-		return (<View style={styles.article.articlesFeedContainer}>
+		return (
+			<View style={ styles.article.articlesFeedContainer }>
+				<Filter
+					filters={[ 'utc', 'assos' ]}
+				/>
 				<FlatList
 					ref={(component) => ( this.flatList = component )}
 					data={this.state.articles}
