2d807ad71c4f0ece70f86b1d896a7a95ca2da572
CdRom1 <r.maliach@live.fr>
2018-08-27 19:52:35 +0200

    articles: correction du bug de dimension

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -24,16 +24,19 @@ export default class ArticleComponent extends React.Component {
 			<View style={styles.article.container}>
 				{/***TITRE***/}
 				<View style={styles.article.titleContainer}>
-					<HTML html={'<span style="' + styles.article.title + '">' + this.props.data["title"] + '</span>'} />
-					{/*** on est obligé de mettre un html pour le titre des actus. Même s'il n'y a pas de balise, il y a des entités html (par exemple &amp; -> "&") utilisées souvent pour les accents français. ***/}
+					<HTML
+						html={'<span style="' + styles.article.title + '">' + this.props.data["title"] + '</span>'}
+				
+					 />
+					{/*** on est obligé de mettre un html pour le titre des actus. Même s'il n'y a pas de balise, il y a des entités html (par exemple &amp; -> "&") utilisées souvent pour les accents français.} ***/}
 				</View>
 				<View style={styles.article.contentContainer}>
 					{/***DESCRIPTION***/}
 					{this.state.folded &&
-						<View style={{maxHeight:50, flex:1}}>
+						<View style={{maxHeight:50}}>
 							{this.props.data["description"] ?
-								<Text style={{textAlign: 'center'}}>{this.props.data["description"]}</Text> : 
-								<HTML html={this.props.data["excerpt"]} imagesMaxWidth={Dimensions.get('window').width} />
+								<Text style={{textAlign: 'center', margin:0, padding:0}}>{this.props.data["description"]}</Text> : 
+								<HTML style={{textAlign: 'center', flex:1}} html={this.props.data["excerpt"]} imagesMaxWidth={Dimensions.get('window').width} />
 							}
 						</View>
 					}
@@ -43,7 +46,7 @@ export default class ArticleComponent extends React.Component {
 					}
 				</View>
 				{/***BOUTON DE DEVELOPPEMENT***/}
-				<TouchableHighlight onPress={() => this._toggleFolded() } style={{flex:1, marginTop: 5, height:20, maxWidth: '30%', alignItems: 'center', justifyContent: 'center'}} underlayColor={'#33333333'}>
+				<TouchableHighlight onPress={() => this._toggleFolded() } style={styles.article.buttonContainer} underlayColor={'#33333333'}>
 						<Image style={{height: 15}} resizeMode={'contain'} resizeMethod={'resize'} source={ this.state.folded ? DownBlueDevelopArrow : UpYellowDevelopArrow} />
 				</TouchableHighlight>
 			</View>
