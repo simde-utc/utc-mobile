fd7dbfc93d5d649844c6e5b9221eb428b72f3ff9
CdRom1 <r.maliach@live.fr>
2018-08-28 18:54:57 +0200

    WelcomeMessage : ajout d'une petite instruction "glissez vers la droite pour commencer"

--- src/screens/Welcome/WelcomeMessage.js
+++ src/screens/Welcome/WelcomeMessage.js
@@ -6,12 +6,14 @@ export default class WelcomeMessageScreen extends React.Component {
 	render() {
 		const titleStyle = styles.get('text.yellow', 'text.h0', 'text.center', 'mb.sm');
 		const subtitleStyle = styles.get('text.gray', 'text.h4', 'text.center', 'mb.xl');
+		const subsubtitleStyle = styles.get('text.gray', 'text.h5', 'text.center', 'mb.xs');
 
 		return (
 			<View style={ styles.container.center }>
 				<Image source={require('../../img/logo_utc.png')} style={ styles.img.logoStyle } resizeMode={'center'} />
 				<Text style={ titleStyle }>Bienvenue !</Text>
 				<Text style={ subtitleStyle }>Découvrons ensemble l'application</Text>
+				<Text style={ subsubtitleStyle}>Glissez vers la droite pour commencer</Text>
 			</View>
 		);
 	}
