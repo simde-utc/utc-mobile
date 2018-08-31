a59da64f1cc41feb2f2a79d3c58b41f4a39f8454
CdRom1 <r.maliach@live.fr>
2018-08-31 23:57:48 +0200

    Portail: ajout de routes pour les actions et commentaires

--- src/services/Portail.js
+++ src/services/Portail.js
@@ -21,7 +21,6 @@ export class Portail extends Api {
 	static user = {}
 
 	static notConnectedException = "Tried to call Portail route but not logged in."
-
 	static scopes = [
 		'user-create-info-identity-auth-app', // Tout simplement ajouter la connexion application
 		'user-create-info-identity-auth-cas', // Ajouter la connexion CAS
@@ -30,6 +29,11 @@ export class Portail extends Api {
 		'user-get-calendars',
 		'user-get-events',
 		'user-get-assos',
+		'user-create-articles-actions',
+		'user-edit-articles-actions-user',
+		'user-get-comments-articles',
+		'user-set-comments-articles',
+		
 	]
 
 	constructor() {
@@ -313,6 +317,69 @@ export class Portail extends Api {
 			Api.GET,
 		)
 	}
+
+	getUserArticleActions(uuid) {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'user/articles/' + uuid + '/actions/',
+			Api.GET,
+		)
+	}
+
+	getArticleRootComments(uuid) {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'articles/' + uuid + '/comments/',
+			Api.GET,
+		)
+	}
+
+	setArticleRootComment(uuid, body, visibilityId) {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'articles/' + uuid + '/comments/',
+			Api.POST,
+			{
+				body: body,
+				visibility_id : visibilityId,
+			}
+		)
+	}
+
+	getArticleSubComment(artId, commId) {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'articles/' + artId + '/comments/' + commId + '/comments/',
+			Api.GET,
+		)
+	}
+
+	setArticleSubComment(artId, commId, body, visibilityId) {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'articles/' + artId + '/comments/' + commId + '/comments/',
+			Api.POST,
+			{
+				body: body,
+				visibility_id : visibilityId,
+			},
+		)
+	}
+
+	getPortailVisibilities() {
+		this._checkConnected();
+
+		return this.call(
+			Portail.API_V1 + 'visibilities'
+		)
+
+	}
+
 
 
 }
