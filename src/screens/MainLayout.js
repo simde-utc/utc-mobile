fb39473ea61862222509d4dc5d9015747660022c
CdRom1 <r.maliach@live.fr>
2018-09-12 16:30:57 +0200

    Passage SVG -> PNG car react-native-svg-uri est instable et ne compile pas en release

--- src/screens/MainLayout.js
+++ src/screens/MainLayout.js
@@ -11,16 +11,16 @@ import ArticlesScreen from './Articles'
 
 // Icons
 import Icon from '../components/Icon'
-import HomeOn from '../img/icons/navbar/home-on.svg'
-import HomeOff from '../img/icons/navbar/home-off.svg'
-import NewsOn from '../img/icons/navbar/news-on.svg'
-import NewsOff from '../img/icons/navbar/news-off.svg'
-import EventsOn from '../img/icons/navbar/events-on.svg'
-import EventsOff from '../img/icons/navbar/events-off.svg'
-import NotificationsOn from '../img/icons/navbar/bell-on.svg'
-import NotificationsOff from '../img/icons/navbar/bell-off.svg'
-import HamburgerOn from '../img/icons/navbar/hamburger-on.svg'
-import HamburgerOff from '../img/icons/navbar/hamburger-off.svg'
+import HomeOn from '../img/icons/navbar/home-on.png'
+import HomeOff from '../img/icons/navbar/home-off.png'
+import NewsOn from '../img/icons/navbar/news-on.png'
+import NewsOff from '../img/icons/navbar/news-off.png'
+import EventsOn from '../img/icons/navbar/events-on.png'
+import EventsOff from '../img/icons/navbar/events-off.png'
+import NotificationsOn from '../img/icons/navbar/bell-on.png'
+import NotificationsOff from '../img/icons/navbar/bell-off.png'
+import HamburgerOn from '../img/icons/navbar/hamburger-on.png'
+import HamburgerOff from '../img/icons/navbar/hamburger-off.png'
 
 // DEBUG
 const show = (text) => <View style={ styles.container.center }><Text style={ styles.text.h0 }>{ text }</Text></View>
