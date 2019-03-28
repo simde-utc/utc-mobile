911b971677fda17712f0c82e11d0706b08a91b67
Arthur Martello <arthurmartello@outlook.com>
2019-03-28 17:15:12 +0100

    Replace Drawer Menu by a new made-one in Hamburger's View

---
+++ src/styles/userProfile.js
@@ -0,0 +1,23 @@
+export const userProfile = {
+    view: {
+        backgroundColor: '#007383',
+        padding: 20,
+        alignItems: 'center',
+    },
+
+    image: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: '#fff',
+        marginBottom: 10
+    },
+
+    name: {
+        fontSize: 25,
+        color: '#fff',
+        fontWeight: 'bold',
+        paddingHorizontal: '5%'
+    }
+};
