a489cf76c3ea092959381ab2bbed2200588a4721
Arthur Martello <arthurmartello@outlook.com>
2019-03-28 17:15:12 +0100

    Replace Drawer Menu by a new made-one in Hamburger's View

---
+++ src/styles/navigation.js
@@ -0,0 +1,25 @@
+export const navigation = {
+    scrollView: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#007383',
+    },
+
+    fullWidthButton: {
+        view: {
+            backgroundColor: '#007d94',
+            height: 50,
+            marginTop: 10,
+            paddingHorizontal: '5%',
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+        },
+
+        text: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            color: '#fff'
+        }
+    },
+};
