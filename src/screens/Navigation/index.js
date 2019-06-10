95d246ae2b5e6e60c3ee8bbeea881006bf2adbcc
NastuzziSamy <samy@nastuzzi.fr>
2019-06-10 17:59:29 +0200

    Amélioration du code pour l'affichage des connexions, profil et accueil

--- src/screens/Navigation/index.js
+++ src/screens/Navigation/index.js
@@ -1,55 +1,51 @@
 import React from 'react';
-import {createStackNavigator} from "react-navigation";
-import {ScrollView, Text, TouchableHighlight, View} from "react-native";
-import AssosScreen from "../Assos/Assos";
-import ProfileScreen from "../Profile";
-import Icon from "../../components/Icon";
-import Arrow from '../../img/icons/arrow_yellow.png'
-import styles from '../../styles'
-import {ProfileHeader} from "../Profile/ProfileHeader";
+import { createStackNavigator } from 'react-navigation';
+import { ScrollView, Text, TouchableHighlight, View } from 'react-native';
+import AssosScreen from '../Assos/Assos';
+import ProfileScreen from '../Profile';
+import Icon from '../../components/Icon';
+import Arrow from '../../img/icons/arrow_yellow.png';
+import styles from '../../styles';
+import ProfileHeader from '../Profile/ProfileHeader';
 
-class FullWidthButton extends React.Component {
-    render() {
-        return (
-            <TouchableHighlight onPress={this.props.onPress} underlayColor={'#007383'} activeOpacity={0.7}>
-                <View style={styles.navigation.fullWidthButton.view}>
-                    <Text style={styles.navigation.fullWidthButton.text}>{this.props.name}</Text>
-                    <Icon style={{flex: 1}} image={Arrow}/>
-                </View>
-            </TouchableHighlight>
-        )
-    }
-}
+export const FullWidthButton = ({ onPress, name }) => (
+	<TouchableHighlight onPress={onPress} underlayColor="#007383" activeOpacity={0.7}>
+		<View style={styles.navigation.fullWidthButton.view}>
+			<Text style={styles.navigation.fullWidthButton.text}>{name}</Text>
+			<Icon style={{ flex: 1 }} image={Arrow} />
+		</View>
+	</TouchableHighlight>
+);
 
-export class NavigationScreen extends React.Component {
-    render() {
-        return (
-            <ScrollView style={styles.navigation.scrollView}>
-                <ProfileHeader onPress={() => this.props.navigation.navigate('Profile')}/>
-                <FullWidthButton name={'Mon compte'} onPress={() => this.props.navigation.navigate('Profile')}/>
-                <FullWidthButton name={'Liste des associations'} onPress={() => this.props.navigation.navigate('Assos')}/>
-            </ScrollView>
-        );
-    }
-}
+export const ListScreen = ({ navigation }) => (
+	<ScrollView style={styles.navigation.scrollView}>
+		<ProfileHeader onPress={() => navigation.navigate('Profile')} />
+		<FullWidthButton name="Mon compte" onPress={() => navigation.navigate('Profile')} />
+		<FullWidthButton name="Liste des associations" onPress={() => navigation.navigate('Assos')} />
+	</ScrollView>
+);
 
-export default NavigationNavigator = createStackNavigator({
-    Navigation: {
-        screen: NavigationScreen,
-        navigationOptions: {
-            header: null
-        }
-    },
-    Assos: {
-        screen: AssosScreen,
-        navigationOptions: {
-            headerTitle: 'Associations'
-        }
-    },
-    Profile: {
-        screen: ProfileScreen
-    }
+const NavigationNavigator = createStackNavigator(
+	{
+		Navigation: {
+			screen: ListScreen,
+			navigationOptions: {
+				header: null,
+			},
+		},
+		Assos: {
+			screen: AssosScreen,
+			navigationOptions: {
+				headerTitle: 'Associations',
+			},
+		},
+		Profile: {
+			screen: ProfileScreen,
+		},
+	},
+	{
+		initialRouteName: 'Navigation',
+	}
+);
 
-},{
-    initialRouteName: 'Navigation'
-});
+export default NavigationNavigator;
