91dd1e66bee08e2e61e496cc1778c4c18e1de953
NastuzziSamy <samy@nastuzzi.fr>
2019-06-10 15:29:18 +0200

    Amélioration du code concernant les services

--- src/services/CASAuth.js
+++ src/services/CASAuth.js
@@ -5,13 +5,14 @@
  *
  * @copyright Copyright (c) 2018, SiMDE-UTC
  * @license AGPL-3.0
-**/
+ * */
 
-import { Alert } from 'react-native'
+import { Alert } from 'react-native';
 import { CAS_URL } from '../../config';
 
-import Api from './Api'
-import Storage from './Storage'
+import Api from './Api';
+import PortailApi from './Portail';
+import Storage from './Storage';
 
 class CASAuth extends Api {
 	constructor(url = CAS_URL) {
@@ -23,127 +24,121 @@ class CASAuth extends Api {
 			fetch(super.urlWithQueries(this.baseUrl + request, queries), {
 				method: method || Api.GET,
 				headers: headers || {},
-				body: super.serialize(body)
-			}).then((response) => {
-				if ((validStatus || Api.VALID_STATUS).includes(response.status)) {
-					return response.text().then((text) => {
-						return resolve([text, response.status, response.url])
-					})
-				}
-				else {
-					return response.text().then((text) => {
-						return reject([text, response.status, response.url])
-					})
-				}
-			}).catch((e) => {
-				return reject([e.message, 523, ""])
+				body: super.serialize(body),
 			})
-		})
+				.then(response => {
+					if ((validStatus || Api.VALID_STATUS).includes(response.status)) {
+						return response.text().then(text => {
+							return resolve([text, response.status, response.url]);
+						});
+					}
+					return response.text().then(text => {
+						return reject([text, response.status, response.url]);
+					});
+				})
+				.catch(e => {
+					return reject([e.message, 523, '']);
+				});
+		});
 	}
 
 	isConnected() {
-		return this.tgt && this.tgt.length > 0
+		return this.tgt && this.tgt.length > 0;
 	}
 
 	setData(login, password) {
 		return this.login(login, password).then(() => {
 			return Storage.setSensitiveData('cas', {
-				login: login,
-				password: password,
-			})
-		})
+				login,
+				password,
+			});
+		});
 	}
 
 	forget() {
-		this.tgt = ''
+		this.tgt = '';
 
-		return Storage.removeSensitiveData('cas')
+		return Storage.removeSensitiveData('cas');
 	}
 
-	getData() {
-		return Storage.getSensitiveData('cas')
+	static getData() {
+		return Storage.getSensitiveData('cas');
 	}
 
-	getLogin() {
-		return Storage.getSensitiveData('cas').then((data) => {
-			return data.login
-		})
+	static getLogin() {
+		return Storage.getSensitiveData('cas').then(data => {
+			return data.login;
+		});
 	}
 
 	autoLogin() {
-		return this.getData().then((data) => {
-			return this.login(data.login, data.password).then((response) => {
-				return response
-			}).catch(() => {
-				return PortailApi.forget()
-			})
-		})
+		return this.getData().then(data => {
+			return this.login(data.login, data.password)
+				.then(response => {
+					return response;
+				})
+				.catch(() => {
+					return PortailApi.forget();
+				});
+		});
 	}
 
 	login(login, passwd) {
 		return this.call(
-			"",
+			'',
 			Api.POST,
-			"",
+			'',
 			{
 				username: login,
-				password: passwd
+				password: passwd,
 			},
 			CASAuth.HEADER_FORM_URLENCODED,
-			[ 201 ]
-		).then(([response, status, url]) => {
-			this.tgt = this._parseTgt(response)
+			[201]
+		)
+			.then(([response, status, url]) => {
+				this.tgt = CASAuth.parseTgt(response);
 
-			return [response, status, url]
-		}).catch(this._error)
+				return [response, status, url];
+			})
+			.catch(CASAuth.error);
 	}
 
 	getService(service) {
 		return this.call(
 			this.tgt,
 			Api.POST,
-			"",
+			'',
 			{
-				service: service
+				service,
 			},
 			CASAuth.HEADER_FORM_URLENCODED,
-			[ 200 ]
-		).catch(this._error)
+			[200]
+		).catch(CASAuth.error);
 	}
 
-	_error(e) {
-		if (e instanceof TypeError)
-			return [JSON.stringify(e), 523, ""]
-		else {
-			if (Array.isArray(e) && e.length === 3) {
-				let a, b, c
-				[a, b, c] = e
+	static error(e) {
+		if (e instanceof TypeError) return [JSON.stringify(e), 523, ''];
 
-				return [JSON.stringify(a), JSON.stringify(b), JSON.stringify(c)]
-			}
-			else
-				return ["Erreur réseau", 523, ""]
+		if (Array.isArray(e) && e.length === 3) {
+			const [a, b, c] = e;
+
+			return [JSON.stringify(a), JSON.stringify(b), JSON.stringify(c)];
 		}
+		return ['Erreur réseau', 523, ''];
 	}
 
-	_parseTgt(content) {
+	static parseTgt(content) {
 		try {
-			let start = content.indexOf('tickets//') + 8
-			let end = content.indexOf('"', start)
+			const start = content.indexOf('tickets//') + 8;
+			const end = content.indexOf('"', start);
 
-			return content.substring(start, end)
-		}
-		catch(e) {
-			Alert.alert(
-				'CAS',
-				'Une erreur CAS a été rencontrée',
-				[
-					{ text: 'Continuer' },
-				],
-				{ cancelable: false }
-			)
+			return content.substring(start, end);
+		} catch (e) {
+			Alert.alert('CAS', 'Une erreur CAS a été rencontrée', [{ text: 'Continuer' }], {
+				cancelable: false,
+			});
 		}
 	}
 }
 
-export default new CASAuth()
+export default new CASAuth();
