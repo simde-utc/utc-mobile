e3faa5ef4bf0bef8726fc68847d76ac02282dc4f
NastuzziSamy <samy@nastuzzi.fr>
2019-06-17 15:22:48 +0200

    Indication de l'affichage des catégories dans les FAQs

--- src/screens/FAQs/Categories.js
+++ src/screens/FAQs/Categories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, FlatList, ScrollView, View } from 'react-native';
+import { Alert, SectionList, ScrollView, View, Text } from 'react-native';
 import PortailApi from '../../services/Portail';
 import styles from '../../styles';
 import FakeItem from '../../components/FakeItem';
@@ -33,18 +33,35 @@ export default class CategoriesScreen extends React.PureComponent {
 			.then(categories => this.setState({ loading: false, categories }))
 			.catch(reason => {
 				console.warn(reason);
+
 				Alert.alert(
 					'Foire aux questions non disponible',
 					'Une erreur est survenue lors de la récupération des questions.',
 					[{ text: 'OK', onPress: () => navigation.goBack() }]
 				);
+
 				this.setState({ loading: false });
 			});
 	}
 
+	getSections() {
+		const { categories } = this.state;
+
+		return [
+			{
+				title: 'Catégories',
+				data: categories
+					.filter(category => category.parent == null)
+					.map(category => {
+						return { key: category.id, category };
+					}),
+			},
+		];
+	}
+
 	render() {
 		const { navigation } = this.props;
-		const { loading, categories } = this.state;
+		const { loading } = this.state;
 
 		if (loading)
 			return (
@@ -54,30 +71,33 @@ export default class CategoriesScreen extends React.PureComponent {
 			);
 
 		return (
-			<FlatList
-				style={styles.scrollable.list}
-				data={categories
-					.filter(category => category.parent == null)
-					.map(category => {
-						return { key: category.id, category };
-					})}
-				renderItem={({ item }) => {
-					return (
-						<Category
-							category={item.category}
-							onPress={() => {
-								navigation.navigate({
-									key: item.key,
-									routeName: 'Questions',
-									params: { category: item.category },
-								});
-							}}
-						/>
-					);
-				}}
-				ItemSeparatorComponent={() => <View style={styles.scrollable.itemSeparator} />}
-				ListEmptyComponent={() => <FakeItem title={"Aucune question n'a été trouvée"} />}
-			/>
+			<View>
+				<SectionList
+					style={styles.scrollable.list}
+					sections={this.getSections()}
+					renderItem={({ item }) => {
+						return (
+							<Category
+								category={item.category}
+								onPress={() => {
+									navigation.navigate({
+										key: item.key,
+										routeName: 'Questions',
+										params: { category: item.category },
+									});
+								}}
+							/>
+						);
+					}}
+					renderSectionHeader={({ section: { title } }) => (
+						<View style={styles.scrollable.sectionHeader.view}>
+							<Text style={styles.scrollable.sectionHeader.title}>{title}</Text>
+						</View>
+					)}
+					ItemSeparatorComponent={() => <View style={styles.scrollable.itemSeparator} />}
+					ListEmptyComponent={() => <FakeItem title={"Aucune question n'a été trouvée"} />}
+				/>
+			</View>
 		);
 	}
 }
