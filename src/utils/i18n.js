1153ce64255291bf501c2159fc2e2a5f5d821381
NastuzziSamy <samy@nastuzzi.fr>
2019-06-18 14:08:51 +0200

    Ajout de méthodes de traduction

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -7,24 +7,41 @@
  * @copyright Copyright (c) 2019, SiMDE-UTC
  * @license GPL-3.0
  */
-import i18nJs from 'i18n-js';
+import i18n from 'i18n-js';
 
 import fr from '../locales/fr.json';
 import en from '../locales/en.json';
 
-i18nJs.defaultLocale = 'en';
-i18nJs.fallbacks = true;
-i18nJs.translations = { en, fr };
+i18n.defaultLocale = 'en';
+i18n.fallbacks = true;
+i18n.translations = { en, fr };
 
 export const getTranslationsFor = defaultPath => {
 	return (path, params) => {
-		return i18nJs.t(`${defaultPath}.${path}`, params);
+		return i18n.t(`${defaultPath}.${path}`, params);
 	};
 };
 
-const i18n = {
-	getTranslationsFor,
-	t: i18nJs.t,
+// Retourne un texte avec son premier caractère en majuscule.
+export const T = (path, params) => {
+	const string = i18n.t(path, params);
+
+	return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+// Retourne un mot avec son premier caractère en majuscule.
+export const _ = (path, params) => {
+	return T(`words.${path}`, params);
+};
+
+// Retourne un mot avec son premier caractère en majuscule.
+export const e = (path, params) => {
+	return i18n.t(`errors.${path}`, params);
+};
+
+i18n.getTranslationsFor = getTranslationsFor;
+i18n.T = T;
+i18n._ = _;
+i18n.e = e;
 
 export default i18n;
