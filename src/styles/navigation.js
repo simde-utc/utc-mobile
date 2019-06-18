ec8aa4bbe23c40cd6ba6df62a0bc1bd5e42db0d6
NastuzziSamy <samy@nastuzzi.fr>
2019-06-18 02:26:45 +0200

    Ajout des icones sur les contacts et amélioration du clique

--- src/styles/navigation.js
+++ src/styles/navigation.js
@@ -13,9 +13,7 @@ export default {
 		},
 
 		view: {
-			paddingVertical: 7,
-			paddingLeft: 5,
-			paddingRight: 7,
+			padding: 7,
 			backgroundColor: '#fff',
 			flex: 1,
 			flexDirection: 'row',
@@ -23,9 +21,6 @@ export default {
 		},
 
 		textView: {
-			borderLeftWidth: 2,
-			borderLeftColor: '#fff',
-			paddingLeft: 2,
 			flex: 1,
 		},
 
@@ -40,9 +35,7 @@ export default {
 		},
 
 		backView: {
-			paddingVertical: 7,
-			paddingLeft: 7,
-			paddingRight: 5,
+			padding: 7,
 			flex: 1,
 			backgroundColor: '#fff',
 			flexDirection: 'row',
@@ -50,9 +43,6 @@ export default {
 		},
 
 		textBackView: {
-			borderRightWidth: 2,
-			borderRightColor: '#fff',
-			paddingRight: 2,
 			flex: 1,
 		},
 
