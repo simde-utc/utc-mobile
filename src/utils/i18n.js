4538cd0fd03e2e5fc253cba6a271f38f90019eac
NastuzziSamy <samy@nastuzzi.fr>
2019-06-18 15:40:11 +0200

    Ajout de la traduction pour toutes les pages

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -7,24 +7,24 @@
  * @copyright Copyright (c) 2019, SiMDE-UTC
  * @license GPL-3.0
  */
-import i18n from 'i18n-js';
+import i18nJs from 'i18n-js';
 
 import fr from '../locales/fr.json';
 import en from '../locales/en.json';
 
-i18n.defaultLocale = 'en';
-i18n.fallbacks = true;
-i18n.translations = { en, fr };
+i18nJs.defaultLocale = 'en';
+i18nJs.fallbacks = true;
+i18nJs.translations = { en, fr };
 
 export const getTranslationsFor = defaultPath => {
 	return (path, params) => {
-		return i18n.t(`${defaultPath}.${path}`, params);
+		return i18nJs.t(`${defaultPath}.${path}`, params);
 	};
 };
 
 // Retourne un texte avec son premier caractère en majuscule.
 export const T = (path, params) => {
-	const string = i18n.t(path, params);
+	const string = i18nJs.t(path, params);
 
 	return string.charAt(0).toUpperCase() + string.slice(1);
 };
@@ -36,12 +36,26 @@ export const _ = (path, params) => {
 
 // Retourne un mot avec son premier caractère en majuscule.
 export const e = (path, params) => {
-	return i18n.t(`errors.${path}`, params);
+	return i18nJs.t(`errors.${path}`, params);
 };
 
-i18n.getTranslationsFor = getTranslationsFor;
-i18n.T = T;
-i18n._ = _;
-i18n.e = e;
+// Liste des screens avec des traductions.
+export const AppLoader = getTranslationsFor('screens.AppLoader');
+export const Articles = getTranslationsFor('screens.Articles');
+export const Associations = getTranslationsFor('screens.Associations');
+export const Connected = getTranslationsFor('screens.Connected');
+export const Connection = getTranslationsFor('screens.Connection');
+export const Events = getTranslationsFor('screens.Events');
+export const FAQs = getTranslationsFor('screens.FAQs');
+export const Map = getTranslationsFor('screens.Map');
+export const Welcome = getTranslationsFor('screens.Welcome');
+
+const i18n = {
+	...i18nJs,
+	getTranslationsFor,
+	T,
+	_,
+	e,
+};
 
 export default i18n;
