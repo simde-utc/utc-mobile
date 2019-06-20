88568257b4a5d2b42d0ef7f9ecc2def11f436148
Arthur Martello <arthurmartello@outlook.com>
2019-06-20 23:25:03 +0200

    Contenu des notifications

--- src/components/Notifications/Notification.js
+++ src/components/Notifications/Notification.js
@@ -1,27 +1,46 @@
-/*
+/**
  * Encart de notification
  * @author Arthur Martello <arthur.martello@etu.utc.fr>
  *
  * @copyright Copyright (c) 2019, SiMDE-UTC
- * @license AGPL-3.0
+ * @license GPL-3.0
  */
 
-import React from 'react'
-import { Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, TouchableHighlight, Linking } from 'react-native';
 import styles from '../../styles';
 
 export default class Notification extends React.PureComponent {
+	openLink() {
+		const { data } = this.props;
+		const url = data.item.data.action;
+
+		Linking.canOpenURL(url).then(supported => {
+			if (supported) {
+				Linking.openURL(url);
+			}
+		});
+	}
+
 	render() {
-		const {data} = this.props;
+		const { data } = this.props;
 
 		return (
-			<View style={styles.scrollable.item.view}>
-				<View style={{flex: 1}}>
-					<Text style={styles.scrollable.item.title}>Titre</Text>
-					<Text style={styles.scrollable.item.subsubtitle}>Sous-titre</Text>
+			<TouchableHighlight
+				onPress={() => this.openLink()}
+				style={styles.scrollable.item.largeView}
+				underlayColor="#fff"
+			>
+				<View style={{ flex: 1 }}>
+					<Text style={styles.scrollable.item.subsubtitle}>{data.item.created_at}</Text>
+
+					<Text style={styles.scrollable.item.title}>{data.item.data.content}</Text>
+
+					<Text style={styles.scrollable.item.subtitle}>
+						{data.item.data.action.name} : {data.item.data.action.url}
+					</Text>
 				</View>
-			</View>
+			</TouchableHighlight>
 		);
 	}
 }
-
--- src/screens/Notifications/NotificationsScreen.js
+++ src/screens/Notifications/NotificationsScreen.js
@@ -1,20 +1,18 @@
-/*
+/**
  *
  * @author Arthur Martello <arthur.martello@etu.utc.fr>
  *
  * @copyright Copyright (c) 2019, SiMDE-UTC
- * @license AGPL-3.0
+ * @license GPL-3.0
  */
 
-
 import React from 'react';
-import { _, Notifications as n } from '../../utils/i18n';
-import { FlatList, Text, View } from 'react-native';
+import { Alert, FlatList, View, ScrollView } from 'react-native';
+import { _, e, Notifications as t } from '../../utils/i18n';
 import styles from '../../styles';
-import ArticleComponent from '../../components/Articles/Article';
-import PortailApi from '../../services/Portail';
 import FakeItem from '../../components/FakeItem';
 import Notification from '../../components/Notifications/Notification';
+import PortailApi from '../../services/Portail';
 
 export default class NotificationsScreen extends React.PureComponent {
 	static navigationOptions = () => ({
@@ -30,20 +28,47 @@ export default class NotificationsScreen extends React.PureComponent {
 		super(props);
 
 		this.state = {
-			notifications: []
-		}
+			notifications: [],
+			loading: true,
+		};
+	}
+
+	componentDidMount() {
+		this.getNotifications();
+	}
+
+	getNotifications() {
+		const { navigation } = this.props;
+
+		PortailApi.getUserNotifications()
+			.then(notifications => this.setState({ notifications, loading: false }))
+			.catch(reason => {
+				console.warn(reason);
+				Alert.alert(e('associations_not_available'), e('get_association_error'), [
+					{ text: _('ok'), onPress: () => navigation.goBack() },
+				]);
+			});
 	}
 
 	render() {
-		const {notifications} = this.state;
+		const { loading } = this.state;
+
+		if (loading)
+			return (
+				<ScrollView style={styles.scrollable.list}>
+					<FakeItem title={_('loading')} />
+				</ScrollView>
+			);
+
+		const { notifications } = this.state;
 
 		return (
 			<FlatList
 				style={styles.scrollable.list}
 				data={notifications}
-				renderItem={item => <Notification data={item}/>}
-				ItemSeparatorComponent={() => <View style={styles.scrollable.sectionSeparator}/>}
-				ListEmptyComponent={() => <FakeItem title={n('no_notifications')}/>}
+				renderItem={item => <Notification data={item} key={item.id} />}
+				ItemSeparatorComponent={() => <View style={styles.scrollable.sectionSeparator} />}
+				ListEmptyComponent={() => <FakeItem title={t('no_notifications')} />}
 			/>
 		);
 	}
