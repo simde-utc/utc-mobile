08933f7c836acd3a42f8f16c3aba283380c7dcd4
NastuzziSamy <samy@nastuzzi.fr>
2019-06-21 01:12:59 +0200

    Ajout de la gestion des préférences

---
+++ src/utils/Preferences.js
@@ -0,0 +1,20 @@
+/**
+ * Permet de gérer les préférences de l'utilisateur.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2019, SiMDE-UTC
+ * @license GPL-3.0
+ */
+
+const Preferences = {
+	SHOW_UTC_ACTUALITIES: true,
+
+	SHOW_ASSOS_ACTUALITIES: true,
+
+	IS_UTC_BDE_MEMBER: true,
+
+	LANG: 'fr',
+};
+
+export default Preferences;
