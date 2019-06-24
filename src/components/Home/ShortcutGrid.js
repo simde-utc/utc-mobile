109947c4a3eae4e3b5c4ec780766cbe88a47b78c
NastuzziSamy <samy@nastuzzi.fr>
2019-06-24 16:03:17 +0200

    Amélioration de la génération des shortcuts

--- src/components/Home/ShortcutGrid.js
+++ src/components/Home/ShortcutGrid.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FlatList } from 'react-native';
 
 import Shortcut from './Shortcut';
+import BlankShortcut from './BlankShortcut';
+import AssociationShortcut from './AssociationShortcut';
 import Preferences from '../../utils/Preferences';
 import styles from '../../styles';
 
@@ -13,7 +15,11 @@ export default class ShortcutGrid extends React.Component {
 
 		switch (type) {
 			case 'blank':
-				ShortcutType = Shortcut;
+				ShortcutType = BlankShortcut;
+				break;
+
+			case 'association':
+				ShortcutType = AssociationShortcut;
 				break;
 
 			case 'shortcut':
@@ -25,10 +31,26 @@ export default class ShortcutGrid extends React.Component {
 		return <ShortcutType {...(data || {})} navigation={navigation} />;
 	}
 
+	getShortcuts() {
+		const shortcuts = Preferences.USER_SHORTCUTS.slice();
+
+		if (Preferences.USER_SHORTCUTS.length % 2 === 1) {
+			shortcuts.push({
+				type: 'blank',
+			});
+		}
+
+		for (const key in shortcuts) {
+			shortcuts[key].id = key;
+		}
+
+		return shortcuts;
+	}
+
 	render() {
 		return (
 			<FlatList
-				data={Preferences.USER_SHORTCUTS}
+				data={this.getShortcuts()}
 				keyExtractor={shortcut => shortcut.id}
 				renderItem={({ item }) => this.renderShortcut(item)}
 				style={styles.shortcut.gridView}
