05219cdc99ab3e0534b33f57c7913ddc488a6092
Thibault <thibault.picard@etu.utc.fr>
2020-01-24 17:17:24 +0100

    Add a display of the received notifications

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,102 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
+import { Platform } from 'react-native';
+import { AsyncStorage } from 'react-native';
+import { Alert } from 'react-native';
 
 import ArticlesCaroussel from '../../components/Home/ArticlesCaroussel';
 import ShortcutGrid from '../../components/Home/ShortcutGrid';
 import styles from '../../styles';
 
-const Home = ({ navigation }) => (
+import firebase from 'react-native-firebase';
+
+const fcmChannelID = 'fcm_default_channel';
+
+export default class Home extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	async componentDidMount() {
+		this.checkNotificationsPermission();
+		this.addNotificationListeners();
+	}
+
+	componentWillUnmount(){
+		this.removeNotificationListeners();
+	}
+
+	checkNotificationsPermission() {
+		console.log("checkNotificationsPermission");
+		firebase.messaging().hasPermission().then(
+			enabled => {
+				if(enabled) {
+					this.getToken();
+
+				} else {
+					this.requestPermission();
+				}
+			}
+		);
+	}
+
+	async getToken() {
+		let fcmToken = await AsyncStorage.getItem('fcmToken');
+		console.log(fcmToken);
+		if (!fcmToken) {
+			fcmToken = await firebase.messaging().getToken();
+			if (fcmToken) {
+				await AsyncStorage.setItem('fcmToken', fcmToken);
+				// Here save the association between token and utc login in a data base
+			}
+		}
+	}
+
+	async requestPermission() {
+		try {
+			await firebase.messaging().requestPermission();
+			this.getToken();
+		} catch (error) {
+			console.warn('permission rejected');
+		}
+	}
+
+	addNotificationListeners() {
+		this.notificationInitialListener = firebase.notifications().getInitialNotification().then((notificationOpen) => {
+			console.log("initialNotification");
+			const { data } = notificationOpen.notification;
+			if (data.body != null) {
+				Alert.alert("Résultat enseignement", data.body);
+			}
+		});
+
+		this.notificationListener = firebase.notifications().onNotification((notification) => {
+			console.warn("onNotification");
+			Alert.alert("Résultat enseignement",notification.body);
+		});
+	}
+
+	removeNotificationListeners() {
+		this.notificationInitialListener();
+		this.notificationListener();
+	}
+
+	render() {
+		const { navigation } = this.props;
+		return(
+			<ScrollView style={styles.bg.background}>
+				<ArticlesCaroussel navigation={navigation} />
+				<ShortcutGrid navigation={navigation} />
+			</ScrollView>
+		);
+	}
+}
+/*const Home = ({ navigation }) => (
 	<ScrollView style={styles.bg.background}>
 		<ArticlesCaroussel navigation={navigation} />
 		<ShortcutGrid navigation={navigation} />
 	</ScrollView>
 );
 
-export default Home;
+export default Home;*/
